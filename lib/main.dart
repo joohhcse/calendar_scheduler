6b3c2c9ded97ec4a99623f890a47a7c62649c801
hh.joo <joohhcse@gmail.com>
2024-01-14 22:28:43 +0900

    Add new feature: add database

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
+
+  final database = LocalDatabase();  // ➊ 데이터베이스 생성
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);  // ➋ GetIt에
 
   runApp(
     MaterialApp(
